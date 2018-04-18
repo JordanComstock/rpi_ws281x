03511e9fd325dd789d7e101e7aa170525d40b183
Sara Tibbetts <sxt4866@rit.edu>
2018-04-18 14:07:30 -0400

    modifies bluetooth receive to handle stop code

--- python/bluetooth_receive.py
+++ python/bluetooth_receive.py
@@ -1,5 +1,6 @@
 import json
 import bluetooth
+import threading
 import timed_LEDs
 
 
@@ -12,10 +13,16 @@ server_socket.listen(1)
 client_socket, address = server_socket.accept()
 print("Accepted connection from ", address)
 
+threads = []
 while True:
     data = client_socket.recv(1024)
     data = json.loads(data.decode())
-
-    timed_LEDs.start_LEDs(int(data["delay"]), data["lap_times"])
+    if(data["lap_times"]):
+        t = threading.Thread(target=start_laps(int(data["delay"]), data["lap_times"]))
+        threads.append(t)
+        t.start()
 
 client_socket.close()
+
+def start_laps(delay, lap_times):
+    timed_LEDs.start_LEDs(delay, lap_times)
